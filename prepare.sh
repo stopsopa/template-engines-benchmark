7d07873aef6b61c927ff2dc8df4e44ac9f6b2d6c
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-04-06 22:58:37 +0100

    shorter lists

--- prepare.sh
+++ prepare.sh
@@ -1,15 +1,34 @@
 
 set -e
+set -o pipefail
+
+tar_trimmed() {
+    local output
+    local lines
+    local count
+    output=$("$@" 2>&1)
+    local exit_code=$?
+    lines=$(echo "$output" | wc -l | tr -d ' ')
+    count=5
+    if [ "$lines" -le $((count * 2)) ]; then
+        echo "$output"
+    else
+        echo "$output" | head -n "$count"
+        echo "... ($(( lines - count * 2 )) lines omitted) ..."
+        echo "$output" | tail -n "$count"
+    fi
+    return $exit_code
+}
 
 if [ -d lodash ]; then
     echo "${0}: lodash is already extracted"
-else 
+else
     echo "${0}: extracting lodash"
     cd node_modules
-    tar -zcvf lodash.tar.gz lodash
+    tar_trimmed tar -zcvf lodash.tar.gz lodash
     mv lodash.tar.gz ..
     cd ..
-    tar -zxvf lodash.tar.gz
+    tar_trimmed tar -zxvf lodash.tar.gz
     rm -rf lodash.tar.gz
 fi
 
